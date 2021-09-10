b470c85a797e231313b20dd4de890d1e5d8ab339
younyikim <rlakuku1221@gmail.com>
2021-09-10 20:53:56 +0900

    happy birthday countdown : 매년 반복 기능 추가

--- script.js
+++ script.js
@@ -4,27 +4,33 @@ const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 
 let currentYear = new Date().getFullYear();
-const newYear = new Date(`September 24 ${currentYear} 00:00:00`);
-// let newYear = new Date(`September 10 ${currentYear} 19:46:00`);
+let birthday = new Date(`September 24 ${currentYear} 00:00:00`);
+// let birthday = new Date(`September 10 ${currentYear} 20:51:50`);
 
 // 내년 날짜 표시
 const nextYear = document.getElementById("nextYear");
-const thisYear = newYear.getFullYear();
+const thisYear = birthday.getFullYear();
 nextYear.innerHTML = currentYear;
 
-function resetYear() { }
+function resetYear() {
+    birthday = new Date(`September 10 ${currentYear + 1} 19:46:00`);
+    setTimeout(() => {
+        nextYear.innerHTML = currentYear + 1
+        nextYear.style.fontSize = "200px";
+        nextYear.style.color = "rgba(255, 255, 255, 0.315)";
+    }, 86400000);
+
+}
 
 function newYearCountdown(intervalId) {
     const currentDate = new Date();
-    const time = newYear - currentDate;
+    const time = birthday - currentDate;
 
     if (time <= 0) {
         nextYear.style.fontSize = "2.2rem";
         nextYear.style.color = "white";
-        nextYear.innerHTML = `민영이의 ${thisYear - 1998 + 1
-            }번째 생일을 축하해.늘 언니의 행복을 바라는 연이가.`;
-
-        // setTimeout(resetYear, 5000);
+        nextYear.innerHTML = `민영이의 ${thisYear - 1998 + 1}번째 생일을 축하해.늘 언니의 행복을 바라는 연이가.`;
+        resetYear();
     } else {
         const day = Math.floor(time / 1000 / 60 / 60 / 24);
         const hour = Math.floor(time / 1000 / 60 / 60) % 24;
