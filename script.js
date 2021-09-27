edf7e8e4633669985be5d0a1669044322142fe1a
younyikim <rlazmzm1123@gmail.com>
2021-09-27 10:42:32 +0900

    birthday countdown : 남은 날짜 계산 logic 수정

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const title = document.getElementById("title");
 const bg = document.querySelector(".bg");
 
 let currentYear = new Date().getFullYear();
-let yourName = '민영';
-let yourBirthday = 'September 24';
+let yourName = "민영";
+let yourBirthday = "September 24";
 let birthday = new Date(`${yourBirthday} ${currentYear} 00:00:00`);
 title.innerHTML = `${yourName}언니의 생일까지`;
 
@@ -19,11 +19,9 @@ nextYear.innerHTML = currentYear;
 // 생일이 지나면, 내년 생일로 카운트 다운을 초기화한다.
 function resetYear() {
     birthday = new Date(`${yourBirthday} ${currentYear + 1} 00:00:00`);
-    setTimeout(() => {
-        nextYear.innerHTML = currentYear + 1
-        nextYear.style.fontSize = "200px";
-        nextYear.style.color = "rgba(255, 255, 255, 0.315)";
-    }, 86400000);
+    nextYear.innerHTML = currentYear + 1;
+    nextYear.style.fontSize = "200px";
+    nextYear.style.color = "rgba(255, 255, 255, 0.315)";
 }
 
 // 남은 날짜 게산
@@ -34,8 +32,10 @@ function birthdayCounter() {
     if (time <= 0) {
         nextYear.style.fontSize = "2.2rem";
         nextYear.style.color = "white";
-        nextYear.innerHTML = `${yourName}이의 ${thisYear - 1998 + 1}번째 생일을 축하해. 오늘 하루 종일 행복하고, 재밌게 놀자! 무덤까지 함께하는거야 키키 사랑해!!! 연이가`;
-        resetYear();
+        nextYear.innerHTML = `${yourName}이의 ${
+            thisYear - 1998 + 1
+        }번째 생일을 축하해. 오늘 하루 종일 행복하고, 재밌게 놀자! 무덤까지 함께하는거야 키키 사랑해!!! 연이가`;
+        setTimeout(() => resetYear(), 86400000);
     } else {
         const day = Math.floor(time / 1000 / 60 / 60 / 24);
         const hour = Math.floor(time / 1000 / 60 / 60) % 24;
@@ -52,14 +52,14 @@ function birthdayCounter() {
 // 5초마다 배경 이미지 변경
 function changeImg() {
     let imageIdx = Math.floor(Math.random() * 10);
-    document.body.style.backgroundImage= `url("./image/background${imageIdx}.jpg")`;
+    document.body.style.backgroundImage = `url("./image/background${imageIdx}.jpg")`;
     setTimeout(changeImg, 5000);
 }
 
 // 이미지 변경시, Flicker 방지를 위한 preload
 let images = [];
 function preload() {
-    for(let i = 0; i < preload.arguments.length; i++) {
+    for (let i = 0; i < preload.arguments.length; i++) {
         images[i] = new Image();
         images[i].src = preload.arguments[i];
     }
@@ -76,7 +76,7 @@ preload(
     "./image/background7.jpg",
     "./image/background8.jpg",
     "./image/background9.jpg"
-)
+);
 
 // 1초마다 countdown 함수 실행
 setInterval(birthdayCounter, 1000);
